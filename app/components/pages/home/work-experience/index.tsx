6b2c8809547cb7c539d9b9228e817321823da166
Victtor777 <victtor.eng.software@gmail.com>
2024-07-15 21:04:00 -0300

    Add Experience Section e responsividade

--- app/components/pages/home/work-experience/index.tsx
+++ app/components/pages/home/work-experience/index.tsx
@@ -3,7 +3,7 @@ import { ExperienceItem } from "./experience-item";
 
 export const WorkExperience = () => {
   return (
-    <section className="container py-16 flex gap-16">
+    <section className="container py-16 flex flex-col gap-10 md:gap-4 lg:gap-16 md:flex-row">
       <div className="max-w-[420px]">
         <SectionTitle
           subtitle="experiências"
