1e18c1294d177869a864a9718c69dc9895685204
Victtor777 <victtor.eng.software@gmail.com>
2024-07-20 20:15:49 -0300

    Add a função para gerar páginas estáticas do projeto

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -56,7 +56,7 @@ export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
             {homeInfo.introTechnologies.map((tech) => (
-              <TechBadge name={tech.name} />
+              <TechBadge key={tech.name} name={tech.name} />
             ))}
           </div>
 
