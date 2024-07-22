108247faaea747f9b0ee0eeedce64964759796f8
Victtor777 <victtor.eng.software@gmail.com>
2024-07-22 14:35:55 -0300

    Add animações ao Hero

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -13,6 +13,7 @@ import {
 import { HomePageInfo } from "@/app/types/page-info";
 import { RichText } from "@/app/components/rich-text";
 import { CMSIcon } from "@/app/components/cms-icon";
+import { motion } from "framer-motion";
 
 // const MOCK_CONTACTS = [
 //   {
@@ -47,7 +48,14 @@ export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
   return (
     <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
-        <div className="w-full lg:max-w-[530px]">
+        <motion.div
+          className="w-full lg:max-w-[530px]"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          // viewport={{ once: true }}
+        >
           <p className="font-mono text-emerald-400">Olá, meu nome é</p>
           <h2 className="text-4xl font-medium mt-2">Gabriel Borges</h2>
           <div className="text-gray-400 my-6 text-sm sm:text-base">
@@ -55,8 +63,15 @@ export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
           </div>
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
-            {homeInfo.introTechnologies.map((tech) => (
-              <TechBadge key={tech.name} name={tech.name} />
+            {homeInfo.introTechnologies.map((tech, i) => (
+              <TechBadge
+                key={tech.name}
+                name={tech.name}
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ duration: 0.2, delay: i * 0.1 }}
+              />
             ))}
           </div>
 
@@ -79,15 +94,23 @@ export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
               ))}
             </div>
           </div>
-        </div>
+        </motion.div>
 
-        <Image
-          width={420}
-          height={404}
-          src={homeInfo.profilePicture.url}
-          alt="Foto de perfil"
-          className="w-[300px] h=[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: 200, scale: 0.5 }}
+          transition={{ duration: 0.5 }}
+          className="origin-center"
+        >
+          <Image
+            width={420}
+            height={404}
+            src={homeInfo.profilePicture.url}
+            alt="Foto de perfil"
+            className="w-[300px] h=[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
+          />
+        </motion.div>
       </div>
     </section>
   );
