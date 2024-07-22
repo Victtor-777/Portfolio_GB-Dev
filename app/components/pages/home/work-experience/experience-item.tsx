392582b842645fc676410221be37f28459ee1a59
Victtor777 <victtor.eng.software@gmail.com>
2024-07-22 16:35:20 -0300

    Add todas as animações da página principal

--- app/components/pages/home/work-experience/experience-item.tsx
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import logoCompany from "/public/images/logo-company.png";
 import { TechBadge } from "@/app/components/tech-badge";
@@ -5,6 +7,8 @@ import { WorkExperience } from "@/app/types/work-experience";
 import { RichText } from "@/app/components/rich-text";
 import { differenceInMonths, differenceInYears, format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { motion } from "framer-motion";
+import { techBadgeAnimation } from "@/app/lib/animations";
 
 type ExperienceItemProps = {
   experience: WorkExperience;
@@ -44,7 +48,13 @@ export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
       : `${months} mes${months > 1 ? "es" : ""}`;
 
   return (
-    <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
+    <motion.div
+      className="grid grid-cols-[40px,1fr] gap-4 md:gap-10"
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="flex flex-col items-center gap-4">
         <div className="rounded-full border border-gray-500 p-0.5">
           <Image
@@ -81,14 +91,16 @@ export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
           Competências
         </p>
         <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
-          {technologies.map((tech) => (
+          {technologies.map((tech, i) => (
             <TechBadge
               key={`experience-${companyName}-tech-${tech.name}`}
               name={tech.name}
+              {...techBadgeAnimation}
+              transition={{ duration: 0.2, delay: i * 0.1 }}
             />
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
