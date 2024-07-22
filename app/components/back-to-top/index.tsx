6307caa51bebb9d5d8ae7156900c4dc1fb303b37
Victtor777 <victtor.eng.software@gmail.com>
2024-07-22 17:56:31 -0300

    Animação do scroll to up e finalização do projeto

--- app/components/back-to-top/index.tsx
+++ app/components/back-to-top/index.tsx
@@ -3,6 +3,7 @@
 import { TbArrowNarrowUp } from "react-icons/tb";
 import { Button } from "../button";
 import { useCallback, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const BackToTop = () => {
   const [show, setShow] = useState(false);
@@ -12,8 +13,11 @@ export const BackToTop = () => {
   };
 
   const handleScroll = useCallback(() => {
-    if (!show && window.scrollY > 500) setShow(true);
-    if (!show && window.scrollY <= 500) setShow(false);
+    if (window.scrollY > 500 && !show) {
+      setShow(true);
+    } else if (window.scrollY <= 500 && show) {
+      setShow(false);
+    }
   }, [show]);
 
   useEffect(() => {
@@ -22,17 +26,22 @@ export const BackToTop = () => {
   }, [handleScroll]);
 
   return (
-    <>
+    <AnimatePresence>
       {show && (
-        <div className="fixed right-4 bottom-4 z-20">
+        <motion.div
+          className="fixed right-4 bottom-4 z-20"
+          initial={{ opacity: 0, right: -10 }}
+          animate={{ opacity: 1, right: 16 }}
+          exit={{ opacity: 0, right: -10 }}
+        >
           <Button
             onClick={scrollToTop}
             className="shadow-lg shadow-emerald-400/20"
           >
             <TbArrowNarrowUp size={20} />
           </Button>
-        </div>
+        </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 };
